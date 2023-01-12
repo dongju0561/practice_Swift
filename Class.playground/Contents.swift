a4bbb81705fb193eba0be72e5808910197f69920
DongjuPark <pdj95044@gmail.com>
2023-01-12 19:29:36 +0900

    learning about class

--- Class.playground/Contents.swift
+++ Class.playground/Contents.swift
@@ -1,10 +1,14 @@
 import UIKit
+import Foundation
 
 class GasStation{
-    
     var costDisel: Int
     var costGasolin: Int
-
+    var totalGas: Int{
+        get{
+            return costDisel + costGasolin
+        }
+    }
     init(costDisel: Int, costGasolin: Int){
         self.costDisel = costDisel
         self.costGasolin = costGasolin
@@ -27,6 +31,7 @@ class GasStation{
 var huamGasStation = GasStation(costDisel: 1700, costGasolin: 1500)
 
 var price = huamGasStation.howMuch(select: "Disel", mount: 50)
+print(huamGasStation.totalGas)
 
 print(price)
 
