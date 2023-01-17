5b758791ef67f7502584f3655e87bc95e7b7b23f
DongjuPark <77201628+dongju0561@users.noreply.github.com>
2023-01-17 20:56:44 +0900

    Update Contents.swift

--- Class.playground/Contents.swift
+++ Class.playground/Contents.swift
@@ -2,19 +2,19 @@ import UIKit
 import Foundation
 
 class GasStation{
-    var costDisel: Int
+    var costDisel: Int // 클래스의 프로퍼티
     var costGasolin: Int
-    var totalGas: Int{
+    var totalGas: Int{ // 연산 프로퍼티
         get{
             return costDisel + costGasolin
         }
     }
-    init(costDisel: Int, costGasolin: Int){
+    init(costDisel: Int, costGasolin: Int){ // 생성자 생성시 디젤 가격과, 가솔린 가격 초기화
         self.costDisel = costDisel
         self.costGasolin = costGasolin
     }
     
-    func howMuch(select: String, mount: Int) -> Int{
+    func howMuch(select: String, mount: Int) -> Int{ // 클래스의 메소드눈 문자열과 정수를 인자로 받아 int형태로 반환
         if(select == "Disel"){
             return mount * costDisel
         }
